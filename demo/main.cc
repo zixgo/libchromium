0151294f151c10641fb11d898c0e8cc036d1ee2b
zixgo <33917964+zixgo@users.noreply.github.com>
2023-03-25 14:20:07 +0800

    Update demo source

--- demo/main.cc
+++ demo/main.cc
@@ -1,11 +1,23 @@
-#include <iostream>
+// 参考链接：
+// https://github.com/keyou/chromium_demo/blob/c/110.0.5481/demo.cc
 
-#include "base/bind.h"
-#include "base/functional/bind.h"
-#include "base/time/time.h"
+#include "base/at_exit.h"
+#include "base/command_line.h"
+#include "base/logging.h"
+#include "base/run_loop.h"
+#include "base/task/single_thread_task_executor.h"
+#include "base/task/thread_pool/thread_pool_instance.h"
 
 int main(int argc, char* argv[]) {
-  std::cout << base::Time::Now().ToInternalValue() << std::endl;
-  //   auto callback = base::BindOnce();
+  base::AtExitManager at_exit;
+  base::CommandLine::Init(argc, argv);
+  logging::SetLogItems(true, false, true, false);
+
+  base::SingleThreadTaskExecutor main_thread_task_executor;
+  base::ThreadPoolInstance::CreateAndStartWithDefaultParams("demo");
+
+  LOG(INFO) << "Hello, World!";
+  base::RunLoop().Run();
+
   return 0;
 }
